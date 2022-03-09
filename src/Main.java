7097a6bff52ed42b758e99b6f0314e5a757098fa
Valci Júnior <valcijunior19@hotmail.com>
2022-03-09 14:39:16 -0300

    criacao das classes Curso e Mentoria

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.com.desafioDio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso c = new Curso("Java para iniciantes", "Aprenda o básico da linguagem java", 12);
+        System.out.println(c);
+    }
+}
